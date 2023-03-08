37ef3bf0d22c01958d749440b6b312bacb7c813c
Dick Brown <rab@stolaf.edu>
2023-03-08 17:43:36 -0500

    add development version of pdc_task.php

---
+++ pdc_task.php
@@ -0,0 +1,49 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/* ==============================================================
+ *
+ * PDC
+ *
+ * ==============================================================
+ *
+ * @copyright  2014 Richard Lobb, University of Canterbury
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * adapted from cpp_task.php by rab@stolaf.edu, March 2023
+ */
+
+require_once('application/libraries/LanguageTask.php');
+
+class PDC_Task extends Task {
+
+    public function __construct($filename, $input, $params) {
+        parent::__construct($filename, $input, $params);
+        $this->default_params['compileargs'] = array(
+            '-Wall',
+            '-Werror');
+    }
+
+    public static function getVersionCommand() {
+        return array('echo 0.1', '/([0-9.]*)/');
+    }
+
+    public function compile() {
+        $this->executableFileName = $this->sourceFileName;
+    }
+
+
+    // A default name for C++ programs
+    public function defaultFileName($sourcecode) {
+        return 'prog';
+    }
+
+
+    // The executable is the output from the compilation
+    public function getExecutablePath() {
+        return "/usr/bin/bash";
+    }
+
+
+    public function getTargetFile() {
+        return $this->sourceFileName;
+    }
+};
